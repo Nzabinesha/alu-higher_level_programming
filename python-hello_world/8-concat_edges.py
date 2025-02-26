649107fb8b442645325f4039b8b6ccd984dd5a9b
Nzabinesha <m.nzabinesh@alustudent.com>
2025-02-26 00:58:47 -0800

    alu-higher_level_programming/python-hello_world

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-sentence = ("object-oriented programming with Python")
-print(sentence)
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+str = str[39:67] + str[6:12]
+print(str)
