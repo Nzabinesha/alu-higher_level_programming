f860c9988dfcfd846e7726ccc811059ed2d49b95
Nzabinesha <m.nzabinesh@alustudent.com>
2025-02-25 23:50:52 -0800

    alu-higher_level_programming/python-hello_world

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
@@ -5,4 +6,3 @@ middle_word = word[1:-1]
 print(word_first_3)
 print(word_last_2)
 print(middle_word)
-
--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
+#!/usr/bin/python3
 sentence = ("object-oriented programming with Python")
 print(sentence)
-
