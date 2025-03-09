2c747c3fa39a8d01ded3c408646f4e2ec7c46e31
Nzabinesha <m.nzabinesh@alustudent.com>
2025-03-08 22:20:42 -0800

    alu-higher_level_programming/python-classes

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -12,3 +12,7 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """ Define area of square"""
+        return (self.__size ** 2)
